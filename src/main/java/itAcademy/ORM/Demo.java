54e4b0fae9a71da4f7de483e8eb7fa61ce32ebfe
dyominov <ersamsungc2500>
2019-10-01 09:57:37 +0300

    Revert "added example in the Demo class for creating connection to DB"
    This reverts commit cfa597c7313bfaec39f4e2230f3aaa5d91e6bc3f.

--- src/main/java/itAcademy/ORM/Demo.java
+++ src/main/java/itAcademy/ORM/Demo.java
@@ -1,20 +1,12 @@
 package itAcademy.ORM;
 
-import itAcademy.ORM.connection.connectionpool.DBCPDataSourceFactory;
-import itAcademy.ORM.support.DaoSupport;
-import itAcademy.ORM.transaction.BaseTransaction;
+import itAcademy.ORM.mapping.util.Util;
 
-import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Demo {
 
-    public static void main(String[] args) {
-        DBCPDataSourceFactory dataSource = new DBCPDataSourceFactory();
-        Connection connection = DaoSupport.getConnection(dataSource.getDataSource());
-        BaseTransaction transaction = new BaseTransaction(connection);
-        transaction.open();
-        System.out.println("Some operations will have to be here ...");
-        transaction.commit();
-        transaction.close();
+    public static void main(String[] args) throws SQLException {
+        Util.generateTables();
     }
 }
