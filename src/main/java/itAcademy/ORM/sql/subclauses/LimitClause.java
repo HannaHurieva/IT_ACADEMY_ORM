8054d99ce13e4d0e8dac84be107fae8ed6a7ec1c
ann <anngurjeva@gmail.com>
2019-10-06 00:34:30 +0300

    created implementation for subclauses: WHERE, ORDER_BY, GROUP_BY, HAVING, LIMIT, JOIN;

---
+++ src/main/java/itAcademy/ORM/sql/subclauses/LimitClause.java
@@ -0,0 +1,30 @@
+package itAcademy.ORM.sql.subclauses;
+
+import itAcademy.ORM.sql.subclauses.ISubclause;
+
+public class LimitClause implements ISubclause {
+
+    private static final String format = "LIMIT %d, %d";
+
+    private int count;
+    private int start;
+
+    public LimitClause(int recordCount){
+        this(recordCount, 0);
+    }
+
+    public LimitClause(int recordCount, int startOffset){
+        this.start = startOffset;
+        this.count = recordCount;
+    }
+
+    @Override
+    public String getClauseFormat() {
+        return format;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(getClauseFormat(), start, count);
+    }
+}
